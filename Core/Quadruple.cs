91e52706b9d742f09063ab2edc1606a7c7ddb82f
Pedro Ramos <pedrowarkenramos@gmail.com>
2024-10-21 17:54:54 -0300

    refactor: to string method in quadruple class

--- Core/Quadruple.cs
+++ Core/Quadruple.cs
@@ -2,7 +2,8 @@ using System.Diagnostics;
 
 namespace ReversibleTuringMachine.Core;
 
-public class Quadruple {
+public class Quadruple
+{
     public int StartState { get; set; }
 
     public int EndState { get; set; }
@@ -17,30 +18,48 @@ public class Quadruple {
     /// </summary>
     public bool IsActive { get; set; } = false;
 
-    public struct TapeActionIn {
+    public struct TapeActionIn
+    {
         public bool Read { get; set; }
         public string SymbolRead { get; set; }
     }
 
-    public struct TapeActionOut {
+    public struct TapeActionOut
+    {
         public bool Write { get; set; }
         public bool Move { get; set; }
         public Direction MoveDirection { get; set; }
         public string SymbolWritten { get; set; }
     }
 
-    public override string ToString() {
-        return $"({StartState}, [{string.Join(", ", ActionIn.Select(x=> !x.Read ? "/" : x.SymbolRead))}])" +
-            $"->" +
-            $"([{string.Join(", ", ActionOut.Select(x => 
-                x.Move ? 
-                    (x.MoveDirection switch {
-                        Direction.None => "0",
-                        Direction.R => "+1",
-                        Direction.L => "-1",
-                        _ => throw new TuringException("Invalid direction to convert to string")
-                    }) : 
-                    (x.Write ? x.SymbolWritten : "?")
-                ))}], {EndState})";
+    public override string ToString()
+    {
+        return $"({StartState}, [{this.GetReadSymbol()}])->([{this.GetMoveDirectionOrReadSymbol()}], {EndState})";
+    }
+
+    private string GetReadSymbol()
+    {
+        return string.Join(", ", ActionIn.Select(x => !x.Read ? "/" : x.SymbolRead));
+    }
+
+    private string GetMoveDirectionOrReadSymbol()
+    {
+        return string.Join(", ", ActionOut.Select(x =>
+        {
+            if (x.Move)
+            {
+                return x.MoveDirection switch
+                {
+                    Direction.None => "0",
+                    Direction.R => "+1",
+                    Direction.L => "-1",
+                    _ => throw new TuringException("Invalid direction to convert to string")
+                };
+            }
+            else
+            {
+                return x.Write ? x.SymbolWritten : "?";
+            }
+        }));
     }
 }
